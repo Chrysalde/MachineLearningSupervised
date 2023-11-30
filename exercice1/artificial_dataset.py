f63ef0fa647f1d7411886a8a56e34ea4ac5fd80e
TheVengeur <45660215+TheVengeur@users.noreply.github.com>
2023-11-30 14:49:41 +0100

    [+] add requirement.txt and finished Exercice1

--- exercice1/artificial_dataset.py
+++ exercice1/artificial_dataset.py
@@ -1,36 +1,45 @@
-# This allows imports from the parent directory
-import sys
-sys.path.append('..')
-from libs.utilities import mkdir
-from time import time
+import pandas as pd
+import numpy as np
+    # This program uses numpy to generate random data and pandas to organize it into a DataFrame. 
 
-def generate_dataset(*, folder: str = '.', filename: str = 'artificial_dataset.csv'):
-    r"""Generates a brand new random dataset for us to play with.
-
-    Explicit Parameters:
-    --------------------
-    folder : str = '.'
-        The path to the folder that shall contain the file.
-
-    filename : str = 'artificial_dataset.csv'
-        The name of the file to create.
-
-    Notes:
-    -----
-    The data within the dataset created will always be separated by semi-colon.
+def generate_dataset():
+    r"""Generate a numerical dataset of 300 datapoints and 6 columns.
+        The first column contains only integers.
+        The second column contains only floats.
+        The third column has a mean close to 2.5.
+        The fourth column is positively correlated.
+        The fifth column is negatively correlated.
+        The sixth one has a correlaction close to 0.
     """
 
-    # Create the folder.
-    mkdir(folder)
+    num_datapoints = 300
+    num_columns = 6
 
-    # Create and open the file for writing
-    file = open(f"{folder}/{filename}", "w")
+    data = {}
 
-    # Write to the file
-    file.write("N/A;N/A")
+    # Column 1: Integers
+    data['col1'] = np.random.randint(1, 10, num_datapoints)
 
-    # Close the file
-    file.close()
+    # Column 2: Floats
+    data['col2'] = np.random.uniform(0, 5, num_datapoints)
+
+    # Column 3: Mean close to 2.5
+    data['col3'] = np.random.normal(loc=2.5, scale=1, size=num_datapoints)
+
+    # Column 4: Positively correlated
+    data['col4'] = np.random.normal(loc=5, scale=2, size=num_datapoints) + 0.5 * data['col1']
+
+    # Column 5: Negatively correlated
+    data['col5'] = np.random.normal(loc=5, scale=2, size=num_datapoints) - 0.5 * data['col2']
+
+    # Column 6: Correlation close to 0
+    data['col6'] = np.random.normal(loc=5, scale=2, size=num_datapoints) + np.random.normal(loc=0, scale=2, size=num_datapoints)
+
+    # Create a DataFrame
+    df = pd.DataFrame(data)
+
+    # Save to CSV
+    df.to_csv('artificial_dataset.csv', index=False)
 
 if __name__ == '__main__':
     print("[EX1] Generating dataset...")
