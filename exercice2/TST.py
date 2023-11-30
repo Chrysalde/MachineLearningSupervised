3a8fb376ace30856454116ac35e25c4e647661e1
Chrysalde Caron <chrysalde.caron@epitech.eu>
2023-11-30 14:31:35 +0100

    [-] signolib

--- exercice2/TST.py
+++ exercice2/TST.py
@@ -1,8 +1,3 @@
 # This allows imports from the parent directory
 import sys
 sys.path.append('..')
-import signo as signo
-
-file = signo.csvFile("./test.csv")
-
-print(file.count())
--- signo.py
+++
@@ -1,7 +0,0 @@
-# This file is not a main script
-if __name__ == '__main__':
-    print("[signo:signo] -- Error --")
-    exit(1)
-
-from signo_impl.csvFile import csvFile
-from signo_impl.SeekingPosition import SeekingPosition
